d6a30f66b3960957930c2d79939822851e15f13d
stefanKnott <stefan.knott@layer3tv.com>
2018-02-10 12:24:17 -0700

    naive implementation of graceful shutdown (process all open reqs before exit))

--- hashServer.go
+++ hashServer.go
@@ -5,12 +5,24 @@ import (
 	"encoding/base64"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
-type hashReq struct {
-	password string
+type env struct {
+	requestStatus chan string //used for tracking open requests
+	shutdown      bool
+	remaining     int
 }
+
+const (
+	POST     string = "POST"
+	PROCESS  string = "PROCESSING"
+	FINISHED string = "FINSIHED"
+	SHUTDOWN string = "SHUTDOWN"
+)
+
+//solution for handle rest of requests on shutdown could include a buffered channel
 
 func getHash(password string) string {
 	hasher := sha512.New()
@@ -20,17 +32,62 @@ func getHash(password string) string {
 	return sha
 }
 
-func processHashRequest(w http.ResponseWriter, r *http.Request) {
+func (env *env) processHashRequest(w http.ResponseWriter, r *http.Request) {
+	env.requestStatus <- PROCESS
+
+	//filter out any request that is not a POST
+	if r.Method != POST {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
 	//get string from request
 	r.ParseForm()
 	hash := getHash(r.Form.Get("password"))
+
 	//per requirements..stall for 5 seconds
 	time.Sleep(5 * time.Second)
 	w.Write([]byte(hash))
+	env.requestStatus <- FINISHED
+}
 
+func (env *env) processShutdownRequest(w http.ResponseWriter, r *http.Request) {
+	//TODO: refactor this out
+	if r.Method != POST {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	//write 200 back to requester
+	w.WriteHeader(http.StatusOK)
+	//send shutdown request
+	env.requestStatus <- SHUTDOWN
+}
+
+func (env *env) graceful() {
+	for req := range env.requestStatus {
+		if req == PROCESS && env.shutdown == false {
+			env.remaining++
+		} else if req == FINISHED {
+			env.remaining--
+		} else if req == SHUTDOWN {
+			env.shutdown = true
+		}
+
+		if env.shutdown && env.remaining == 0 {
+			os.Exit(0)
+		}
+	}
 }
 
 func main() {
-	http.HandleFunc("/hash", processHashRequest)
+	env := new(env)
+	//buffer 100 requests, should be plenty for simple hash
+	env.requestStatus = make(chan string, 100)
+
+	http.HandleFunc("/hash", env.processHashRequest)
+	http.HandleFunc("/shutdown", env.processShutdownRequest)
+	go env.graceful()
+	log.Println("Server listeniing for requests on 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
