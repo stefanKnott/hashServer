39a4b8bc3d09b983875805f3588e68fd07a4d8ce
stefanKnott <stefan.knott@layer3tv.com>
2018-02-10 13:45:42 -0700

    init hash code mapping and stats request handling

--- hashServer.go
+++ hashServer.go
@@ -3,19 +3,31 @@ package main
 import (
 	"crypto/sha512"
 	"encoding/base64"
+	"encoding/json"
+	"hash/fnv"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 )
 
+type statistics struct {
+	Total             int `json:"total"`
+	totalDuration     time.Duration
+	AverageDurationMs float32 `json:"average"`
+}
+
 type env struct {
-	requestStatus chan string //used for tracking open requests
+	requestStatus chan string       //used for tracking open requests
+	idPwHashTable map[uint32]string //id:password hash table
 	shutdown      bool
 	remaining     int
+	Stats         statistics
 }
 
 const (
+	GET      string = "GET"
 	POST     string = "POST"
 	PROCESS  string = "PROCESSING"
 	FINISHED string = "FINSIHED"
@@ -32,23 +44,54 @@ func getHash(password string) string {
 	return sha
 }
 
+//return the average duration of hash requests in milliseconds
+func calculateaverageDurationMs(count int, totalDuration time.Duration) float32 {
+	return float32(totalDuration.Nanoseconds()) / float32(1000000) / float32(count)
+}
+
 func (env *env) processHashRequest(w http.ResponseWriter, r *http.Request) {
 	env.requestStatus <- PROCESS
+	var hash string
+	var hashCode uint32
 
-	//filter out any request that is not a POST
-	if r.Method != POST {
+	env.Stats.Total++
+	start := time.Now()
+	switch r.Method {
+	case GET:
+		id, err := strconv.Atoi(r.URL.Query().Get("id"))
+		if err != nil {
+			//write invalid to requester
+			w.WriteHeader(http.StatusNotImplemented)
+		}
+		//get the hash password
+		w.Write([]byte(env.idPwHashTable[uint32(id)]))
+		return
+	case POST:
+		//get string from request
+		r.ParseForm()
+		hash = getHash(r.Form.Get("password"))
+		hashCode = getHashCode(hash)
+
+		str := strconv.Itoa(int(hashCode))
+		w.Write([]byte(str))
+	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
 
-	//get string from request
-	r.ParseForm()
-	hash := getHash(r.Form.Get("password"))
+	env.requestStatus <- FINISHED
+	env.Stats.totalDuration += time.Since(start)
+	env.Stats.AverageDurationMs = calculateaverageDurationMs(env.Stats.Total, env.Stats.totalDuration)
 
 	//per requirements..stall for 5 seconds
 	time.Sleep(5 * time.Second)
-	w.Write([]byte(hash))
-	env.requestStatus <- FINISHED
+	env.idPwHashTable[hashCode] = hash
+}
+
+func getHashCode(s string) uint32 {
+	h := fnv.New32a()
+	h.Write([]byte(s))
+	return h.Sum32()
 }
 
 func (env *env) processShutdownRequest(w http.ResponseWriter, r *http.Request) {
@@ -80,13 +123,27 @@ func (env *env) graceful() {
 	}
 }
 
+func (env *env) processStatsRequest(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case GET:
+		b, err := json.Marshal(env.Stats)
+		if err != nil {
+			panic(err)
+		}
+		w.Write(b)
+		return
+	}
+}
+
 func main() {
 	env := new(env)
 	//buffer 100 requests, should be plenty for simple hash
 	env.requestStatus = make(chan string, 100)
-
-	http.HandleFunc("/hash", env.processHashRequest)
-	http.HandleFunc("/shutdown", env.processShutdownRequest)
+	env.idPwHashTable = make(map[uint32]string)
+	http.HandleFunc("/hash/", env.processHashRequest)
+	http.HandleFunc("/shutdown/", env.processShutdownRequest)
+	http.HandleFunc("/stats/", env.processStatsRequest)
+	// http.HandleFunc("/hashID", env.processHashRequestByID)
 	go env.graceful()
 	log.Println("Server listeniing for requests on 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
